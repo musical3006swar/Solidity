2ee82add0edbf65b9c261c267862569988cbef23
musical3006swar <4al22cs176@gmail.com>
2025-11-02 10:19:56 +0530

    Updated smart contracts and JSON build files

--- solidityCourse2/Module1/arrays.sol
+++ solidityCourse2/Module1/arrays.sol
@@ -3,15 +3,8 @@ pragma solidity ^0.8.0;
 
 contract changeArray{
     uint[] _changeArray;
-
-    // if(_changeArray.length>0){
-    //     for(uint i=0;i<_changeArray.length;i++){
-    //         if(_changeArray[i]==2)_changeArray.removeElement(i);
-    //     }
-    // }
-    function removeElement(uint i) public {
+    function removeElement(uint i) public{
         _changeArray[i]=_changeArray[_changeArray.length-1];
-        _changeArray.pop();
     }
     function pop() public returns(uint[] memory){
         _changeArray.pop();
@@ -21,8 +14,5 @@ contract changeArray{
         for(uint i=1;i<=4;i++){
             _changeArray.push(i);
         }
-    }
-    function show() public view returns(uint[] memory){
-        return _changeArray;
     }
 }
