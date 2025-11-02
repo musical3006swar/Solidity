1779d68dc2c630cc805ac19903608c35d1b5ca16
musical3006swar <4al22cs176@gmail.com>
2025-11-02 16:14:00 +0530

    Updated smart contracts and JSON build files

---
+++ solidityCourse2/Module1/struct.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract structure{
+    struct Movie{
+        string title;
+        string director;
+        uint id;
+    }
+    Movie movie;
+    function setMovie() public{
+        movie=Movie("Batla House","Swar",3);
+    }
+    function getMovieId() public view returns(uint){
+        return movie.id;
+    }
+
+    function setNewMovie(string memory n, string memory t,uint i) public returns(uint){
+        movie=Movie(n,t,i);
+        return movie.id;
+    }
+    Movie comedy;
+
+}
+
