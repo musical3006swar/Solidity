20e073aa536183303f1df0897e563928f4c50e34
Swar <4al22cs176@gmail.com>
2025-11-19 23:30:13 +0530

    Updated files

--- solidityCourse2/Module3/fallBack.sol
+++ solidityCourse2/Module3/fallBack.sol
@@ -3,10 +3,20 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract fallBack{
     event log(uint gas);
-    fallback() external{
+    fallback() external payable{
         emit log(gasleft());
     }
     function balance() public view returns(uint){
         return address(this).balance;
+    }
+}
+
+contract sendToFallBack{
+    function transferToFallback(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+    function callFallback(address payable _to) public payable {
+        (bool sent,)=_to.call{value:msg.value}('');
+        require(sent,'Failed to send');
     }
 }
