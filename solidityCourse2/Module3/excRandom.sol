dcfb4c9fe88247e8f5ce2854b06ef907cc39193d
Swar <4al22cs176@gmail.com>
2025-11-20 15:27:40 +0530

    Updated files

--- solidityCourse2/Module3/excRandom.sol
+++ solidityCourse2/Module3/excRandom.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+
+contract oracle{
+    address admin;
+    uint public rand;
+    constructor(){
+        admin=msg.sender;
+    }
+    function assign(uint num) public {
+        rand=num;
+        require(admin==msg.sender,"Cannot proceed");
+        
+    }
+}
--- solidityCourse2/Module3/generateRandomNum.sol
+++ solidityCourse2/Module3/generateRandomNum.sol
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+
 contract GenerateRandomNumber{
+    Oracle public oracle;
+    constructor(address _oracleAddress){
+        oracle=Oracle(_oracleAddress);
+    }
     function randGen(uint range) public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.timestamp,block.prevrandao,msg.sender)))%range;//block.prevrandao is completely identical to block.difficulty
+        return uint(keccak256(abi.encodePacked(block.timestamp,block.prevrandao,msg.sender,oracle.rand())))%range;//block.prevrandao is completely identical to block.difficulty
+    }
+}
+
+contract Oracle{
+    address admin;
+    uint public rand;
+    constructor(){
+        admin=msg.sender;
+    }
+    function assign(uint num) public {
+        require(admin==msg.sender,"Cannot proceed");
+        rand=num;
+        
     }
 }
